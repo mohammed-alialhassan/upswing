388091b721048c12c50e04ac546029613f3e23ee
Xavier Martin Demers <xavier.pmd@outlook.com>
2021-11-26 15:25:51 -0500

    Seeded the database with example users

---
+++ db/seeds/01_users.sql
@@ -0,0 +1,7 @@
+-- Insert Example User Seeds Into Database
+
+INSERT INTO users (first_name, last_name, username, email, password) VALUES ('Sebastien', 'Mardem', 'BigMon', 'numayweather@gmail.com', 'password');
+INSERT INTO users (first_name, last_name, username, email, password) VALUES ('Jore', 'Shordina', 'ShortyJor', 'msmoseby@gmail.com', 'password');
+INSERT INTO users (first_name, last_name, username, email, password) VALUES ('Arisa', 'Fukawa', 'foodie131', 'littlebigpicket@gmail.com', 'password');
+INSERT INTO users (first_name, last_name, username, email, password) VALUES ('Mike', 'Wazowski', '2319-sock', 'monstersinc@outlook.com', 'password');
+INSERT INTO users (first_name, last_name, username, email, password) VALUES ('Violet', 'Jinx', 'SistersIGuess', 'silco4life@gmail.com', 'password');
