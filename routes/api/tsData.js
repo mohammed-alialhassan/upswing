2bbb5c4dd53192cbd187a4ef4a65338ff7fdc5a0
Xavier Martin Demers <xavier.pmd@outlook.com>
2021-11-28 17:13:56 -0500

    sorted ts data before outputting json

--- routes/api/tsData.js
+++ routes/api/tsData.js
@@ -12,9 +12,9 @@ router.get(`/`, (req, res) => {
       `
       SELECT
         json_build_object(
-          '${ticker}_ts_daily', (SELECT json_agg(row_to_json(a)) FROM ${ticker}_ts_daily a),
-          '${ticker}_ts_weekly', (SELECT json_agg(row_to_json(b)) FROM ${ticker}_ts_weekly b),
-          '${ticker}_ts_monthly', (SELECT json_agg(row_to_json(c)) FROM ${ticker}_ts_monthly c),
+          '${ticker}_ts_daily', (SELECT json_agg(row_to_json(a) ORDER BY date) FROM ${ticker}_ts_daily a),
+          '${ticker}_ts_weekly', (SELECT json_agg(row_to_json(b) ORDER BY date) FROM ${ticker}_ts_weekly b),
+          '${ticker}_ts_monthly', (SELECT json_agg(row_to_json(c) ORDER BY date) FROM ${ticker}_ts_monthly c),
           '${ticker}_company_overview', (SELECT json_agg(row_to_json(d)) FROM ${ticker}_company_overview d),
           '${ticker}_balance_sheet', (SELECT json_agg(row_to_json(e)) FROM ${ticker}_balance_sheet e)
         )
