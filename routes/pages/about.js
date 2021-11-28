88990d09c68e4118ca49c05e82824406827bf937
Xavier Martin Demers <xavier.pmd@outlook.com>
2021-11-28 04:13:32 -0500

    edited page routes to use res.render

--- routes/pages/about.js
+++ routes/pages/about.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router  = express();
 
 router.get("/", (req, res) => {
-  res.json("about page");
+  res.render();
 });
 
 module.exports = router;
--- routes/pages/screener.js
+++ routes/pages/screener.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router  = express();
 
 router.get("/", (req, res) => {
-  res.json("screener page");
+  res.json();
 });
 
 module.exports = router;
