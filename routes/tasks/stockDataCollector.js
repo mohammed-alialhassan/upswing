50ea26cf4b84a5b4f2d353b453fdd3a73693ff41
Xavier Martin Demers <xavier.pmd@outlook.com>
2021-11-29 17:17:22 -0500

    send user to api page after fetching stock data

--- routes/tasks/stockDataCollector.js
+++ routes/tasks/stockDataCollector.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { externalDataFetcher } = require("../../lib/externalDataFetcher");
 
-router.get("/", (req, res) => {
-  externalDataFetcher("IBM");
+router.get('/', (req, res) => {
+  externalDataFetcher(req.body.ticker);
+  res.redirect('/api/stock-data');
 });
 
 module.exports = router;
