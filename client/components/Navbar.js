e035157d1c3a04f7858421d885373ff5cccb521d
Mohammed Ali Al-Hassan <88508812+mohammed-alialhassan@users.noreply.github.com>
2022-05-19 19:12:54 -0400

    Fixed positioning for navbars

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,69 +1,73 @@
-import React, { Fragment, useState, useContext, useEffect } from 'react'
-import { Menu, Popover, Transition } from '@headlessui/react'
-import { tickerIcon } from '@heroicons/react/solid'
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
-import MenuDropDown from './MenuDropdown'
-import SuccessAlert from './Alerts/success'
-import ErrorAlert from './Alerts/error'
-import axios from 'axios'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { useRouter } from 'next/router';
+import React, { Fragment, useState, useContext, useEffect } from "react";
+import { Menu, Popover, Transition } from "@headlessui/react";
+import { tickerIcon } from "@heroicons/react/solid";
+import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
+import MenuDropDown from "./MenuDropdown";
+import SuccessAlert from "./Alerts/success";
+import ErrorAlert from "./Alerts/error";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useRouter } from "next/router";
 
 const user = {
-  name: 'Chelsea Hagon',
-  email: 'chelsea.hagon@example.com',
+  name: "Chelsea Hagon",
+  email: "chelsea.hagon@example.com",
   imageUrl:
-    'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-}
+    "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+};
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Calendar', href: '#', current: false },
-  { name: 'Teams', href: '#', current: false },
-  { name: 'Directory', href: '#', current: false },
-]
+  { name: "Dashboard", href: "#", current: true },
+  { name: "Calendar", href: "#", current: false },
+  { name: "Teams", href: "#", current: false },
+  { name: "Directory", href: "#", current: false },
+];
 const userNavigation = [
-  { name: 'Your Profile', href: '#' },
-  { name: 'Settings', href: '#' },
-  { name: 'Sign out', href: '#' },
-]
+  { name: "Your Profile", href: "#" },
+  { name: "Settings", href: "#" },
+  { name: "Sign out", href: "#" },
+];
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 export default function Navbar({ isLoggedIn }) {
-
-  const [ ticker, setTicker ] = useState('');
+  const [ticker, setTicker] = useState("");
   const router = useRouter();
 
   const handleClick = () => {
-  
-    axios.post('http://localhost:3001/stock-data-collector', {
-      ticker: ticker
-    }).then(result => {
+    axios
+      .post("http://localhost:3001/stock-data-collector", {
+        ticker: ticker,
+      })
+      .then((result) => {
+        setTimeout(() => {
+          axios
+            .post("http://localhost:3001/api/stock-data", {
+              ticker: ticker,
+            })
+            .then((result) => {
+              const tickerData = result.data;
+              router.push(
+                {
+                  pathname: `[${ticker}]`,
+                  // query: {  }
+                  query: {
+                    name: ticker,
+                    data: JSON.stringify(tickerData),
+                  },
+                },
+                `[${ticker}]`
+              );
+            });
+        }, 3000);
 
-      setTimeout(() => {
-        axios.post('http://localhost:3001/api/stock-data', {
-        ticker: ticker
-        }).then(result => {
-        const tickerData = result.data;
-        router.push({
-         pathname: `[${ticker}]`,
-        // query: {  }
-        query: { 
-          name: ticker, 
-          data: JSON.stringify(tickerData)
-        }
-       }, `[${ticker}]`);
-  
-        })
-      }, 3000)
-
-      toast.success('Your search is in process!');
-    }).catch(err => {
-      toast.error('Sorry! Please login first!');
-    })
+        toast.success("Your search is in process!");
+      })
+      .catch((err) => {
+        toast.error("Sorry! Please login first!");
+      });
   };
 
   return (
@@ -73,60 +77,85 @@ export default function Navbar({ isLoggedIn }) {
         as="header"
         className={({ open }) =>
           classNames(
-            open ? 'fixed inset-0 z-40 overflow-y-auto' : '',
-            'bg-white shadow-sm lg:static lg:overflow-y-visible'
+            open ? "fixed inset-0 z-40 overflow-y-auto" : "",
+            "bg-white shadow-sm lg:static lg:overflow-y-visible"
           )
         }
       >
         {({ open }) => (
           <>
-          <ToastContainer
-position="top-right"
-autoClose={5000}
-hideProgressBar={false}
-newestOnTop={false}
-closeOnClick
-rtl={false}
-pauseOnFocusLoss
-draggable
-pauseOnHover
-/>
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-              <div className="relative flex justify-between xl:grid xl:grid-cols-12 lg:gap-8">
+            <ToastContainer
+              position="top-right"
+              autoClose={5000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+            />
+            <div className="max-w-screen-2xl mx-auto px-2 sm:px-2 lg:px-0">
+              <div className="relative flex justify-between lg:gap-8">
                 <div className="flex md:absolute md:left-0 md:inset-y-0 lg:static xl:col-span-2">
                   <div className="flex-shrink-0 flex items-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-24" viewBox="0 0 20 20" fill="currentColor">
-                  <a href="/home" className="fill-stone-600 hover:fill-amber-300">
-                  <path fill-rule="evenodd" d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z" clip-rule="evenodd" />
-                  <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z" />
-                  </a>
-                  </svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-14 w-28"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <a
+                        href="/home"
+                        className="fill-stone-600 hover:fill-amber-300"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z"
+                          clip-rule="evenodd"
+                        />
+                        <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z" />
+                      </a>
+                    </svg>
                   </div>
                 </div>
-                <div className="min-w-0 flex-1 md:px-8 lg:px-0 xl:col-span-6">
+                <div className="min-w-0 flex-1  md:px-8 lg:px-0 xl:col-span-6">
                   <div className="flex items-center px-6 py-4 md:max-w-3xl md:mx-auto lg:max-w-none lg:mx-0 xl:px-0">
-                    <div className="w-full">
+                    <div className="w-full ml-80">
                       <label htmlFor="ticker" className="sr-only">
                         Search
                       </label>
-                      <div className="relative">
+                      <div className="relative max-w-xl">
                         <div className="min-w-full pointer-events-none absolute inset-y-0  pl-3 flex items-center">
-                          <tickerIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                          
-                          <div className='min-w-full flex justify-end'>
+                          <tickerIcon
+                            className="h-5 w-5 text-gray-400"
+                            aria-hidden="true"
+                          />
 
-                          
-                           <button
-        type="button"
-        className=" h-9 px-3 my-5 z-50 pointer-events-auto border border-gray-300 shadow-sm text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        onClick={handleClick}
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-  <path strokeLinecap="round" strokeLinejoin="round" d="M8 16l2.879-2.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242zM21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-</svg>
-      </button></div>
+                          <div className="min-w-full flex justify-end">
+                            <button
+                              type="button"
+                              className=" h-9 px-3 my-5 z-50 pointer-events-auto border border-gray-300 shadow-sm text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                              onClick={handleClick}
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-6 w-6"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                                strokeWidth={2}
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  d="M8 16l2.879-2.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242zM21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                                />
+                              </svg>
+                            </button>
+                          </div>
                         </div>
-                        
+
                         <input
                           id="ticker"
                           name="ticker"
@@ -134,10 +163,8 @@ pauseOnHover
                           placeholder="Search..."
                           type="text"
                           value={ticker}
-                          onChange={event => setTicker(event.target.value)}
+                          onChange={(event) => setTicker(event.target.value)}
                         />
-
-                        
                       </div>
                     </div>
                   </div>
@@ -153,14 +180,14 @@ pauseOnHover
                     )}
                   </Popover.Button>
                 </div>
-               
-                <div className='order-last'>
-                
-                <MenuDropDown isLoggedIn={isLoggedIn} /></div>
+
+                <div className="order-last">
+                  <MenuDropDown isLoggedIn={isLoggedIn} />
+                </div>
               </div>
             </div>
 
-           {/* {ticker !== '' && click !== 0? (
+            {/* {ticker !== '' && click !== 0? (
               <SuccessAlert />
            ) : (<ErrorAlert />)} */}
             <Popover.Panel as="nav" className="lg:hidden" aria-label="Global">
@@ -169,10 +196,12 @@ pauseOnHover
                   <a
                     key={item.name}
                     href={item.href}
-                    aria-current={item.current ? 'page' : undefined}
+                    aria-current={item.current ? "page" : undefined}
                     className={classNames(
-                      item.current ? 'bg-gray-100 text-gray-900' : 'hover:bg-gray-50',
-                      'block rounded-md py-2 px-3 text-base font-medium'
+                      item.current
+                        ? "bg-gray-100 text-gray-900"
+                        : "hover:bg-gray-50",
+                      "block rounded-md py-2 px-3 text-base font-medium"
                     )}
                   >
                     {item.name}
@@ -182,11 +211,19 @@ pauseOnHover
               <div className="border-t border-gray-200 pt-4 pb-3">
                 <div className="max-w-3xl mx-auto px-4 flex items-center sm:px-6">
                   <div className="flex-shrink-0">
-                    <img className="h-10 w-10 rounded-full" src={user.imageUrl} alt="" />
+                    <img
+                      className="h-10 w-10 rounded-full"
+                      src={user.imageUrl}
+                      alt=""
+                    />
                   </div>
                   <div className="ml-3">
-                    <div className="text-base font-medium text-gray-800">{user.name}</div>
-                    <div className="text-sm font-medium text-gray-500">{user.email}</div>
+                    <div className="text-base font-medium text-gray-800">
+                      {user.name}
+                    </div>
+                    <div className="text-sm font-medium text-gray-500">
+                      {user.email}
+                    </div>
                   </div>
                   <button
                     type="button"
@@ -213,5 +250,5 @@ pauseOnHover
         )}
       </Popover>
     </>
-  )
+  );
 }
--- client/components/SecondNavbar.js
+++ client/components/SecondNavbar.js
@@ -1,39 +1,38 @@
-import React from 'react'
-import { Menu, Popover, Transition } from '@headlessui/react'
-import { tickerIcon } from '@heroicons/react/solid'
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
-import MenuDropDown from './MenuDropdown'
-import SuccessAlert from './Alerts/success'
-import ErrorAlert from './Alerts/error'
-import axios from 'axios'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { useRouter } from 'next/router';
+import React from "react";
+import { Menu, Popover, Transition } from "@headlessui/react";
+import { tickerIcon } from "@heroicons/react/solid";
+import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
+import MenuDropDown from "./MenuDropdown";
+import SuccessAlert from "./Alerts/success";
+import ErrorAlert from "./Alerts/error";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useRouter } from "next/router";
 
 const user = {
-  name: 'Chelsea Hagon',
-  email: 'chelsea.hagon@example.com',
+  name: "Chelsea Hagon",
+  email: "chelsea.hagon@example.com",
   imageUrl:
-    'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-}
+    "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+};
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Calendar', href: '#', current: false },
-  { name: 'Teams', href: '#', current: false },
-  { name: 'Directory', href: '#', current: false },
-]
+  { name: "Dashboard", href: "#", current: true },
+  { name: "Calendar", href: "#", current: false },
+  { name: "Teams", href: "#", current: false },
+  { name: "Directory", href: "#", current: false },
+];
 const userNavigation = [
-  { name: 'Your Profile', href: '#' },
-  { name: 'Settings', href: '#' },
-  { name: 'Sign out', href: '#' },
-]
+  { name: "Your Profile", href: "#" },
+  { name: "Settings", href: "#" },
+  { name: "Sign out", href: "#" },
+];
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 export default function SecondNavbar(props) {
-
   const router = useRouter();
 
   // Passing down isLoggedIn state from homepage
@@ -46,37 +45,49 @@ export default function SecondNavbar(props) {
         as="header"
         className={({ open }) =>
           classNames(
-            open ? 'fixed inset-0 z-40 overflow-y-auto' : '',
-            'bg-white shadow-sm lg:static lg:overflow-y-visible'
+            open ? "fixed inset-0 z-40 overflow-y-auto" : "",
+            "bg-white shadow-sm lg:static lg:overflow-y-visible"
           )
         }
       >
         {({ open }) => (
           <>
-          <ToastContainer
-position="top-right"
-autoClose={5000}
-hideProgressBar={false}
-newestOnTop={false}
-closeOnClick
-rtl={false}
-pauseOnFocusLoss
-draggable
-pauseOnHover
-/>
+            <ToastContainer
+              position="top-right"
+              autoClose={5000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+            />
             <div className="max-w-screen-2xl mx-auto px-2 sm:px-2 lg:px-0">
               <div className="relative flex justify-between">
                 <div className="flex md:absolute md:left-0 md:inset-y-0 lg:static ">
                   <div className="flex-shrink-0 flex items-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="h-14 w-28" viewBox="0 0 20 20" fill="currentColor">
-                  <a href="/home" className="fill-stone-600 hover:fill-amber-300">
-                  <path fill-rule="evenodd" d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z" clip-rule="evenodd" />
-                  <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z" />
-                  </a>
-                  </svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-14 w-28"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <a
+                        href="/home"
+                        className="fill-stone-600 hover:fill-amber-300"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z"
+                          clip-rule="evenodd"
+                        />
+                        <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z" />
+                      </a>
+                    </svg>
                   </div>
                 </div>
-         
+
                 <div className="flex items-center md:absolute md:right-0 md:inset-y-0 lg:hidden">
                   {/* Mobile menu button */}
                   <Popover.Button className="-mx-2 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
@@ -88,9 +99,9 @@ pauseOnHover
                     )}
                   </Popover.Button>
                 </div>
-                <div className='justify-end order-last'>
-                
-                <MenuDropDown isLoggedIn={isLoggedIn} /></div>
+                <div className="justify-end order-last">
+                  <MenuDropDown isLoggedIn={isLoggedIn} />
+                </div>
               </div>
             </div>
             <Popover.Panel as="nav" className="lg:hidden" aria-label="Global">
@@ -99,10 +110,12 @@ pauseOnHover
                   <a
                     key={item.name}
                     href={item.href}
-                    aria-current={item.current ? 'page' : undefined}
+                    aria-current={item.current ? "page" : undefined}
                     className={classNames(
-                      item.current ? 'bg-gray-100 text-gray-900' : 'hover:bg-gray-50',
-                      'block rounded-md py-2 px-3 text-base font-medium'
+                      item.current
+                        ? "bg-gray-100 text-gray-900"
+                        : "hover:bg-gray-50",
+                      "block rounded-md py-2 px-3 text-base font-medium"
                     )}
                   >
                     {item.name}
@@ -112,11 +125,19 @@ pauseOnHover
               <div className="border-t border-gray-200 pt-4 pb-3">
                 <div className="max-w-3xl mx-auto px-4 flex items-center sm:px-6">
                   <div className="flex-shrink-0">
-                    <img className="h-10 w-10 rounded-full" src={user.imageUrl} alt="" />
+                    <img
+                      className="h-10 w-10 rounded-full"
+                      src={user.imageUrl}
+                      alt=""
+                    />
                   </div>
                   <div className="ml-3">
-                    <div className="text-base font-medium text-gray-800">{user.name}</div>
-                    <div className="text-sm font-medium text-gray-500">{user.email}</div>
+                    <div className="text-base font-medium text-gray-800">
+                      {user.name}
+                    </div>
+                    <div className="text-sm font-medium text-gray-500">
+                      {user.email}
+                    </div>
                   </div>
                   <button
                     type="button"
@@ -143,5 +164,5 @@ pauseOnHover
         )}
       </Popover>
     </>
-  )
+  );
 }
